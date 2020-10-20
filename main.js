ac23f0c641b9c88fa928e0b8db0ac0855b04794c
Ethan-Pongon <pongon.eth@gmail.com>
2020-10-19 20:15:57 -0700

    added sidebar CSS and the start of a homepage that has the sidebar on it

---
+++ main.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const port=process.env.PORT || 8081;
+
+var server = app.listen(port);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/views" + "/" + "home.html");
+});
+
+app.get('/sidebar.css', function(req, res) {
+    res.sendFile(__dirname + "/CSS" + "/" + "sidebar.css");
+})
