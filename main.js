d95fcb16ef747505cdd22653fa9d51df0feabc40
Ethan-Pongon <pongon.eth@gmail.com>
2020-10-28 17:12:23 -0700

    added login/account creation page that recieves gives a username and password to a node function that can be linked to the user accounts system

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+const { info } = require('console');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const port=process.env.PORT || 8081;
 
 var server = app.listen(port);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + "/views" + "/" + "home.html");
@@ -20,4 +23,20 @@ app.get('/home.css', function(req, res) {
 
 app.get('/About', function(req, res) {
     res.sendFile(__dirname + "/views" + "/" + "about.html");
+});
+
+app.post('/go', function(req, res) {
+    res.sendFile(__dirname + "/views" + "/" + "login.html");
+});
+
+app.post('/login', function(req, res) {
+    console.log("login");
+    console.log(req.body.username);
+    console.log(req.body.password);
+});
+
+app.post('/createacc', function(req, res) {
+    console.log("account creation");
+    console.log(req.body.newusername);
+    console.log(req.body.newpassword);
 });
